d24a04227ea05478ec32ad92d192067b296ca299
Stuitjeo <ost21@uclive.ac.nz>
2025-03-26 18:51:25 +1300

    update

--- coin_collector.service.worker.js
+++ coin_collector.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1742967896|26533268857';
+const CACHE_VERSION = '1742968218|26855449941';
 /** @type {string} */
 const CACHE_PREFIX = '2D-Games-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
